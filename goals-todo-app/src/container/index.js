9f972a5e11f569a1477a142de1e3859c21d3b661
Shalu Kumari <shaluk@syncroutesoftware.com>
2018-09-03 22:20:30 +0530

    passed container data to component

--- goals-todo-app/src/container/index.js
+++ goals-todo-app/src/container/index.js
@@ -1,16 +1,24 @@
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
 import { addTodoAction, removeTodoAction, toggleTodo } from './action/todos'
+import { addGoalsAction, removeGoalsAction } from './action/goals'
 
 function mapStateToProps(state) {
-  const { todos } = state
+  const { todos, goals } = state
   return {
-    todos
+    todos,
+    goals
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addTodoAction, removeTodoAction, toggleTodo}, dispatch)
+  return bindActionCreators({
+    addTodoAction,
+    removeTodoAction,
+    toggleTodo,
+    addGoalsAction,
+    removeGoalsAction,
+  }, dispatch)
 }
 
 const Container = connect(mapStateToProps, mapDispatchToProps)
