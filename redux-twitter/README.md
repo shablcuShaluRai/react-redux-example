d57619621f7c3161b3872d832bc53bd61f8ab7b9
Shalu Kumari <shaluk@syncroutesoftware.com>
2018-09-04 22:49:23 +0530

    Redux-twitter

---
+++ redux-twitter/README.md
@@ -0,0 +1,5 @@
+ # Redux twitter
+
+ - This project is for beginner who wants to understand concept of Redux.
+
+ { WORK IN PROGRESS... }
---
+++ redux-twitter/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Redux twitter </h1>
+         <p>Developed By ShablcuShaluRai</p>
+      </div>
+    );
+  }
+}
+
+export default App;
